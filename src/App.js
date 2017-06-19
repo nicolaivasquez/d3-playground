9605431ca1d18c1858f75f54a69db2b7c662ae7c
Nicolai Vasquez <nrvasquez@gmail.com>
2017-06-19 20:10:45 +0100

    add placeholder scenes and routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import RealTimeLineChart from './components/RealTimeLineChart';
+import Home from './components/Home';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openDrawer: false,
+    }
+  }
+  handleToggleDrawer = () => this.setState({
+    openDrawer: !this.state.openDrawer
+  });
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Router>
+          <div className="App">
+            <AppBar
+              title={
+                <Link to="/">D3 Playground</Link>
+              }
+              iconClassNameRight="muidocs-icon-navigation-expand-more"
+              onLeftIconButtonTouchTap={this.handleToggleDrawer}
+            />
+            <Drawer
+              open={this.state.openDrawer}
+              docked={false}
+              onRequestChange={(openDrawer) => this.setState({openDrawer})}
+            >
+              <MenuItem
+                containerElement={<Link to="/real-time-line-chart" />}
+              >Real Time Line Chart</MenuItem>
+            </Drawer>
+
+            <Route exact path="/" component={Home} />
+            <Route path="/real-time-line-chart" component={RealTimeLineChart} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/components/RealTimeLineChart/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RealTimeLineChart extends Component {
+  render() {
+    return (
+      <div>
+        Real Time Line Chart
+      </div>
+    );
+  }
+}
+
+export default RealTimeLineChart;
